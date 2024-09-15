dbd97302e17b4afbdb7b4f298daf1a0d1ca40c8a
KyannaKrueger <kyannakrueger@gmail.com>
2024-09-15 12:45:56 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+function App() {
+  return (
+    <div>
+      <main>
+        <h1>My Tunes</h1>
+        <div className="content">
+          <img src="https://i.pinimg.com/originals/7c/5b/d5/7c5bd54db74b5cebfcbdf65e52a06f43.jpg" alt="melanie martinez" />
+          <div className="albums">
+            <h2>Melanie Martinez Albums</h2>
+            <details>
+              <summary>Crybaby</summary>
+              <ol>
+                <li>Crybaby</li>
+                <li>Dollhouse</li>
+                <li>Sippy Cup</li>
+                <li>Carousel</li>
+                <li>Alphabet Boy</li>
+                <li>Soap</li>
+                <li>Training Wheels</li>
+                <li>Pity Party</li>
+                <li>Tag, You're It</li>
+                <li>Milk and Cookies</li>
+                <li>Pacify Her</li>
+                <li>Mrs. Potato Head</li>
+                <li>Mad Hatter</li>
+              </ol>
+            </details>
+            <details>
+              <summary>K-12</summary>
+              <ol>
+                <li>Wheels On The Bus</li>
+                <li>Class Fight</li>
+                <li>The Principal</li>
+                <li>Show and Tell</li>
+                <li>Nurse's Office</li>
+                <li>Drama Club</li>
+                <li>Strawberry Shortcake</li>
+                <li>Lunchbox Friends</li>
+                <li>Orange Juice</li>
+                <li>Detention</li>
+                <li>Teacher's Pet</li>
+                <li>High School Sweethearts</li>
+                <li>Recess</li>
+              </ol>
+            </details>
+            <details>
+              <summary>Portals</summary>
+              <ol>
+                <li>DEATH</li>
+                <li>VOID</li>
+                <li>TUNNEL VISION</li>
+                <li>FAIREE SOIREE</li>
+                <li>LIGHT SHOWER</li>
+                <li>SPIDER WEB</li>
+                <li>LEECHES</li>
+                <li>BATTLE OF THE LARYNX</li>
+                <li>THE CONTORTIONIST</li>
+                <li>MOON CYCLE</li>
+                <li>NYMPHOLOGY</li>
+                <li>EVIL</li>
+                <li>WOMB</li>
+              </ol>
+            </details>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
